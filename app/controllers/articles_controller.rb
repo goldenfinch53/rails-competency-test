6dc9e78af3061b86f4be27d43a0d61469d87f5e4
Greg <greg@tyemill.com>
2019-06-17 11:52:22 -0600

    finished with intial pass, need to confirm stuff is working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,51 @@
 class ArticlesController < ApplicationController
-  # before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @articles_grouped_by_category = Article.includes(:user).all.group_by(&:category)
   end
 
   def show
-    @article = Article.includes(:user).find(params[:id])
+    if user_signed_in?
+      @article = Article.includes(:user).find(params[:id])
+    else
+      redirect_to new_user_registration_url
+    end
   end
 
+  def edit
+    @article = Article.includes(:user).find(params[:id])
+    authorize @article
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    authorize @article
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @article = Article.new
+    @article.user_id = current_user.id
+    authorize @article
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @article = Article.new
+    authorize @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
 end
---
+++ app/policies/article_policy.rb
@@ -0,0 +1,32 @@
+class ArticlePolicy
+  attr_reader :user, :post
+
+  def initialize(user, article)
+    @user = user
+    @article = article
+  end
+
+  def show?
+    user.exists?
+  end
+
+  def edit? 
+    user.editor? && article.user == user
+  end
+
+  def update? 
+    user.editor? && article.user == user
+  end
+
+  def destroy? 
+    user.editor? && article.user == user
+  end
+
+  def create?
+    user.editor?
+  end
+  
+  def new? 
+    user.editor?
+  end
+end
